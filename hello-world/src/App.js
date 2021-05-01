de02f9980e418fa20e54b3ac53ca8b4309bc33c5
sureshmangs <sureshmangs1@gmail.com>
2021-05-02 00:32:22 +0530

    jsx

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Welcome name="Suresh" />
+        <Hello />
       </div>
     )
   }
