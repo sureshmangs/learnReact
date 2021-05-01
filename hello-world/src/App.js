8cdadbf0492fcadc66c88c937bfb743738a00182
sureshmangs <sureshmangs1@gmail.com>
2021-05-01 23:11:38 +0530

    functional component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
-export class App extends Component {
+import Greet from './components/Greet';
+
+class App extends Component {
   render() {
     return (
       <div>
-        <h1>Learn React </h1>
+        <Greet name="Suresh" />
       </div>
     )
   }
