b676cb5c006395880c0ca99710be2fdef8cc349d
sureshmangs <sureshmangs1@gmail.com>
2021-05-04 07:49:55 +0530

    component-mounting-lifecycle-methods

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Form />
+        <LifecycleA />
       </div>
     )
   }
