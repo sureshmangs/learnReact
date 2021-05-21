732b51e1146d649288be17e143d28c9d5472f1e5
sureshmangs <sureshmangs1@gmail.com>
2021-05-21 15:30:25 +0530

    error-boundary

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import PortalDemo from './components/PortalDemo';
 
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <PortalDemo />
+        <ErrorBoundary>
+          <Hero heroName="Batman" />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Superman" />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Joker" />
+        </ErrorBoundary>
       </div>
     )
   }
