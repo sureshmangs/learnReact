22d87229adb5c62a89098d587d4fc890d0d9c06f
sureshmangs <sureshmangs1@gmail.com>
2021-05-22 00:47:20 +0530

    render-props-part2

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,17 +4,34 @@ import './App.css';
 
 import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
+//import User from './components/User';
+import CounterTwo from './components/CounterTwo';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <ClickCounterTwo />
-        <HoverCounterTwo />
+        <CounterTwo render={(count, incrementCount) => (<ClickCounterTwo count={count} incrementCount={incrementCount} />)} />
+        <CounterTwo render={(count, incrementCount) => <HoverCounterTwo count={count} incrementCount={incrementCount} />} />
+
+
+        {/* Method 2 -> Can also be done in this way */}
+
+        {/* <CounterTwo>
+          {(count, incrementCount) => (<ClickCounterTwo count={count} incrementCount={incrementCount} />)}
+        </CounterTwo> */}
+
+
+        {/* Read User.js Component fro better understanding */}
+        {/* <User render={(isLoggedIn) => isLoggedIn ? "Suresh" : "Guest"} /> */}
       </div>
     )
   }
 }
 
 export default App
+
+// Render Props
+// The term "Render Prop" refers to a technique for sharing code between
+// React Component using a propwhose value is a function
--- hello-world/src/components/HoverCounterTwo.js
+++ hello-world/src/components/HoverCounterTwo.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react'
 
 class HoverCounterTwo extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            count: 0
-        }
-    }
-
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1 }
-        })
-    }
-
     render() {
-        const { count } = this.state;
+        const { count, incrementCount } = this.props;
         return (
             <div>
-                <h3 onMouseOver={this.incrementCount}>Clicked {count} times</h3>
+                <h3 onMouseOver={incrementCount}>Hovered {count} times</h3>
             </div>
         )
     }
