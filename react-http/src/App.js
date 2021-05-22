74be1b91348a55736c9d383038322b6f28e8da36
sureshmangs <sureshmangs1@gmail.com>
2021-05-22 16:19:53 +0530

    http-get-request

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import './App.css'
+
+import PostList from './components/PostList';
+
 export class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Learn React HTTP</h1>
+      <div className="App">
+        <PostList />
       </div>
     )
   }
