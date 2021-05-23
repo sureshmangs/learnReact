f37005c9d39fbe8bbb06e0a52f003d79572b0bb4
sureshmangs <sureshmangs1@gmail.com>
2021-05-23 12:53:40 +0530

    useEffect-render

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
 import './App.css'
-
-import HookCounterFour from './components/HookCounterFour'
+// import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
-        <HookCounterFour />
+        {/* <ClassCounterOne /> */}
+        <HookCounterOne />
       </div>
     )
   }
 }
 
 export default App
+
+// useEffect
+
+// The effect hook lets you perform side efeects in functional components
+// It's a close replacement for componentDidMount, componentDidUpdate and componentWillUnmount
