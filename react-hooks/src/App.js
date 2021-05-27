e0d0644cc273e03363ef1be4099ae60e4796bc82
sureshmangs <sureshmangs1@gmail.com>
2021-05-27 12:22:50 +0530

    useCallback-hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 import './App.css'
-import DataFetchingTwo from './components/DataFetchingTwo';
+import ParentComponent from './components/ParentComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <DataFetchingTwo />
+      <ParentComponent />
     </div>
   )
 }
 
 export default App
+
+// useCallback Hook
+
+// useCallback is a hook that will return a memoized version of the callback
+// function that only changes if one of the dependencies has changed
+
+// It is ueful when passing callbacks to optimized child components that rely 
+// on reference equality to prevent unnecessary renders
