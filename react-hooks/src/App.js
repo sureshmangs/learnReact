71f1b650278c6f23ea34f34f5b51c02eef860c34
sureshmangs <sureshmangs1@gmail.com>
2021-05-27 14:19:19 +0530

    useDocumentTitle-custom-hook

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
 import './App.css'
-import ClassTimer from './components/ClassTimer'
-import HookTimer from './HookTimer';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
+
 
 function App() {
   return (
     <div className="App">
-      <ClassTimer />
-      <HookTimer />
+      <DocTitleOne />
+      <DocTitleTwo />
     </div>
   )
 }
 
 export default App
+
+// Custom Hook
+// A custom hook is basically a JavaScript function whose name starts with "use"
+// A custom hook can also call other hooks if required
+
+// Why ?
+// Share logic - Alternative to HOCs and Render Props
