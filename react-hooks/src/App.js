adfd3a496f5c17eb76e827506bb6a420ca90e568
sureshmangs <sureshmangs1@gmail.com>
2021-05-27 13:13:24 +0530

    useRef-hook-part1

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import './App.css'
-import Counter from './components/Counter'
+import FocusInput from './components/FocusInput'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <FocusInput />
     </div>
   )
 }
